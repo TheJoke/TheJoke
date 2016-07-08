db9a2a887c573caa471ed466f3f85661ae852592
Akshat <malikdebnath@gmail.com>
2016-07-08 02:45:52 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    alert("Hey what is up and I hope you like the website");
+}
+
+function greenColor(){
+    document.body.style.backgroundColor = "#008080";
+    console.log("hit green");
+}
+function redColor() {
+    document.body.style.backgroundColor = "#800000";
+    console.log("hit red");
+}
