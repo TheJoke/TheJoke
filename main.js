e18a0dba6e232f59acead13d194fe269d2ae49c2
Akshat <malikdebnath@gmail.com>
2016-07-08 03:02:36 +0000

    second commit

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ window.onload = function () {
     alert("Hey what is up and I hope you like the website");
 }
 
-function greenColor(){
-    document.body.style.backgroundColor = "#008080";
-    console.log("hit green");
+function CoolStuff(){
+    document.body.style.backgroundImage = "url('imgres.jpg')";
+    console.log("imgres.jpg");
 }
 function redColor() {
     document.body.style.backgroundColor = "#800000";
