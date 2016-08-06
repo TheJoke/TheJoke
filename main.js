269f271c4f45d3225ca84fd915f724ede31dd24c
Akshat <malikdebnath@gmail.com>
2016-08-06 00:19:16 +0000

    Added BATMAN

--- main.js
+++ main.js
@@ -3,10 +3,28 @@ window.onload = function () {
 }
 
 function CoolStuff(){
-    document.body.style.backgroundImage = "url('imgres.jpg')";
-    console.log("imgres.jpg");
+    document.body.style.backgroundImage = "url('jpg')";
+    console.log('jpg');
+    document.body.style.backgroundSize= "12%";
+    
 }
-function redColor() {
-    document.body.style.backgroundColor = "#800000";
-    console.log("hit red");
+
+function OtherCoolstuff() {
+    document.body.style.backgroundImage = "url('jpg 2')";
+    console.log("jpg 2");
+    document.body.style.backgroundSize= "12%";
+    
 }
+
+
+
+
+/*
+to push to ur website
+
+git add -A
+
+git commit -m "WRITE MESSAGE HERE"
+
+git push 
+*/
